d8978bf9bdf58ce862e215af465d8c75b9bc776e
jdessy23 <jerry.dessy@accenturefederal.com>
2023-07-13 19:07:34 +0000

    updates

--- server.py
+++ server.py
@@ -212,7 +212,7 @@ def show_account():
 @app.route('/locations')
 def show_locations():
     """Show local pickup locations"""
-if os.environ["LOCATION_FEATURE_ENABLED"] == "True":
+    if os.environ["LOCATION_FEATURE_ENABLED"] == "True":
         pickups = db.session.query(Pickup).filter(Pickup.pickup_id > 1).all()
         return render_template("locations.html", pickups=pickups)
     else:
